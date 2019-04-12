7e4272c7fb6219fa811b7867a30de7fd61e4fdce
starlord <starlord@xhiftcorp.com>
2019-04-12 11:25:35 +0900

    set

---
+++ src/Components/App/AppPresenter.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import Header from "Components/Header";
+import Flex from "styled-flex-component";
+import Notification from "Components/Notification";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+
+const GlobalStyle = createGlobalStyle`
+     ${reset};
+     body{
+          background-color:#ecf0f1;
+     }
+`;
+
+const AppPresenter = () => (
+  <Fragment>
+     <GlobalStyle body/>
+    <Header />
+    <Flex alignCenter full column>
+      <Notification text={"Hello"} seen={false} />
+    </Flex>
+  </Fragment>
+);
+
+export default AppPresenter;
